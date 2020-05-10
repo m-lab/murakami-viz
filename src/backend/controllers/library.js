6fcef22092340e186b8dac95d14cc017358dc019
Josh King <jking@chambana.net>
2020-05-10 17:19:37 -0400

    Build out additional query filters and input validation, fix querybuilder usage in models.

--- src/backend/controllers/library.js
+++ src/backend/controllers/library.js
@@ -1,7 +1,32 @@
 import Router from '@koa/router';
+import moment from 'moment';
+import Joi from '@hapi/joi';
 import { getLogger } from '../log.js';
+import { BadRequestError } from '../../common/errors.js';
 
 const log = getLogger('backend:controllers:library');
+
+const query_schema = Joi.object({
+  start: Joi.number()
+    .integer()
+    .greater(-1),
+  end: Joi.number()
+    .integer()
+    .positive(),
+  asc: Joi.boolean(),
+  sort_by: Joi.string(),
+  from: Joi.string(),
+  to: Joi.string(),
+});
+
+async function validate_query(query) {
+  try {
+    const value = await query_schema.validateAsync(query);
+    return value;
+  } catch (err) {
+    throw new BadRequestError('Unable to validate query: ', err);
+  }
+}
 
 export default function controller(libraries) {
   const router = new Router();
@@ -22,7 +47,30 @@ export default function controller(libraries) {
     log.debug(`Retrieving libraries.`);
     let res;
     try {
-      res = libraries.findAll();
+      const query = await validate_query(ctx.query);
+      let from, to;
+      if (query.from) {
+        const timestamp = moment(query.from);
+        if (timestamp.isValid()) {
+          ctx.throw(400, 'Invalid timestamp value.');
+        }
+        from = timestamp.toISOString();
+      }
+      if (query.to) {
+        const timestamp = moment(query.to);
+        if (timestamp.isValid()) {
+          ctx.throw(400, 'Invalid timestamp value.');
+        }
+        to = timestamp.toISOString();
+      }
+      res = await libraries.find({
+        start: query.start,
+        end: query.end,
+        asc: query.asc,
+        sort_by: query.sort_by,
+        from: from,
+        to: to,
+      });
       ctx.response.body = {
         status: 'success',
         data: res,
@@ -38,7 +86,7 @@ export default function controller(libraries) {
     log.debug(`Retrieving library ${ctx.params.id}.`);
     let library;
     try {
-      library = libraries.findById(ctx.params.id);
+      library = await libraries.findById(ctx.params.id);
       if (library.length) {
         ctx.response.body = { status: 'success', data: library };
         ctx.response.status = 200;
@@ -78,7 +126,7 @@ export default function controller(libraries) {
     log.debug(`Deleting library ${ctx.params.id}.`);
     let library;
     try {
-      library = libraries.delete(ctx.params.id);
+      library = await libraries.delete(ctx.params.id);
       if (library.length) {
         ctx.response.body = { status: 'success', data: library };
         ctx.response.status = 200;
--- src/backend/controllers/system.js
+++ src/backend/controllers/system.js
@@ -1,7 +1,32 @@
 import Router from '@koa/router';
+import moment from 'moment';
+import Joi from '@hapi/joi';
 import { getLogger } from '../log.js';
+import { BadRequestError } from '../../common/errors.js';
 
 const log = getLogger('backend:controllers:system');
+
+const query_schema = Joi.object({
+  start: Joi.number()
+    .integer()
+    .greater(-1),
+  end: Joi.number()
+    .integer()
+    .positive(),
+  asc: Joi.boolean(),
+  sort_by: Joi.string(),
+  from: Joi.string(),
+  to: Joi.string(),
+});
+
+async function validate_query(query) {
+  try {
+    const value = await query_schema.validateAsync(query);
+    return value;
+  } catch (err) {
+    throw new BadRequestError('Unable to validate query: ', err);
+  }
+}
 
 export default function controller(systems) {
   const router = new Router();
@@ -22,7 +47,30 @@ export default function controller(systems) {
     log.debug(`Retrieving systems.`);
     let res;
     try {
-      res = systems.findAll();
+      const query = await validate_query(ctx.query);
+      let from, to;
+      if (query.from) {
+        const timestamp = moment(query.from);
+        if (timestamp.isValid()) {
+          ctx.throw(400, 'Invalid timestamp value.');
+        }
+        from = timestamp.toISOString();
+      }
+      if (query.to) {
+        const timestamp = moment(query.to);
+        if (timestamp.isValid()) {
+          ctx.throw(400, 'Invalid timestamp value.');
+        }
+        to = timestamp.toISOString();
+      }
+      res = await systems.find({
+        start: query.start,
+        end: query.end,
+        asc: query.asc,
+        sort_by: query.sort_by,
+        from: from,
+        to: to,
+      });
       ctx.response.body = {
         status: 'success',
         data: res,
@@ -38,7 +86,7 @@ export default function controller(systems) {
     log.debug(`Retrieving system ${ctx.params.id}.`);
     let system;
     try {
-      system = systems.findById(ctx.params.id);
+      system = await systems.findById(ctx.params.id);
       if (system.length) {
         ctx.response.body = { status: 'success', data: system };
         ctx.response.status = 200;
@@ -78,7 +126,7 @@ export default function controller(systems) {
     log.debug(`Deleting system ${ctx.params.id}.`);
     let system;
     try {
-      system = systems.delete(ctx.params.id);
+      system = await systems.delete(ctx.params.id);
       if (system.length) {
         ctx.response.body = { status: 'success', data: system };
         ctx.response.status = 200;
